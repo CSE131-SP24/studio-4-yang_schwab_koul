df4d8dac4bf2d4a66a0be0d848794dffac0429ca
arnav <arnav@localhost>
2024-02-21 17:12:15 -0600

    kj

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,20 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenColor(StdDraw.BLUE);
+			StdDraw.filledRectangle(0.5, 0.5, 0.5, 0.3);
+			
+		StdDraw.setPenColor(StdDraw.RED);
+			StdDraw.filledSquare(0.1, 0.69, 0.01);
+			
+		StdDraw.setPenColor(StdDraw.WHITE);
+			StdDraw.filledRectangle(0.5, 0.4, 0.05, 0.1);
+		
+			double [] x = {0.4, 0.5, 0.6};
+			double [] y = {0.5, 0.6, 0.5};
+			StdDraw.filledPolygon(x, y);
+		
+		StdDraw.setPenColor(StdDraw.RED);
+		StdDraw.text(0.5, 0.5, "FREEDOM");
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,31 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shape = in.next();
+		int r = in.nextInt();
+		int g = in.nextInt();
+		int b = in.nextInt();
+		boolean filled = in.nextBoolean();
+		
+		
+		
+		if (shape.equals("rectangle")) {
+			double parameterOne = in.nextDouble();
+			double parameterTwo = in.nextDouble();
+			double parameterThree = in.nextDouble();
+			double parameterFour = in.nextDouble();
+			StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+		}
+		
 	}
+}
+
+
+
+
+if (shape.equals("ellipse")) {
+	StdDraw.ellipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+}
+if (shape.equals()) {
+	StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
 }
